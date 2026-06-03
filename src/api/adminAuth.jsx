dbba5db940e3e474cee7e38e8fcd5f166c617069
Alexander <alexanderl96@yahoo.com>
2026-06-03 18:53:21 -0400

    adding fix to login

--- src/api/adminAuth.jsx
+++ src/api/adminAuth.jsx
@@ -1,8 +1,8 @@
-import API_URL from "../api/api_images";
+// import API_URL from "../api/api_images";
 const API = "http://localhost:5001/api/admin-auth";
 
 export const adminLogin = async (data) => {
-  const res = await fetch(`${API_URL}/api/admin-auth/login`, {
+  const res = await fetch(`${API}/api/admin-auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
