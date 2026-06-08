285a9b60255bdd6c34148e0355172419b8740f03
Alexander <alexanderl96@yahoo.com>
2026-06-08 01:10:32 -0400

    Add Netlify configuration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// // import { HashRouter } from "react-router-dom";
+
+// import { BrowserRouter } from "react-router-dom"; // 👈 ADD THIS
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//     <BrowserRouter> {/* 👈 WRAP HERE */}
+//       <App />
+//     </BrowserRouter>
+//   // </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from "react-router-dom"; // 👈 ADD THIS
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  // <React.StrictMode>
-    <BrowserRouter> {/* 👈 WRAP HERE */}
-      <App />
-    </BrowserRouter>
-  // </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
 
 reportWebVitals();
